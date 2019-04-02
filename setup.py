d917399ed3e382887f041d1fa1937ce4988fdc34
stefanrm@au1.ibm.com <backprop!5>
2019-04-02 13:31:28 +1100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vds',
+    version='1.0.0',
+    url='https://github.com/maet3608/vft-data-sanitizer',
+    author='Stefan Maetschke',
+    author_email='stefan.maetschke@gmail.com',
+    description='Remove sensitive information from visual field test data',
+    packages=find_packages(),
+    install_requires=[],
+)
