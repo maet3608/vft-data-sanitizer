63672611ab308b6f7590c786be9a4385b53d1ebd
stefanrm@au1.ibm.com <backprop!5>
2019-04-04 10:56:49 +1100

    docstring added and readme extended

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # vft-data-sanitizer
 
-Removes sensitive information from visual field test data and replaces
-patient names by ids using fuzzy matching.
+Removes sensitive information from XML files containing visual field test data 
+and replaces patient names by number ids using fuzzy matching.
+
+
+# Installation
+
+```
+git clone https://github.com/maet3608/vft-data-sanitizer.git
+cd vft-data-sanitizer
+python setup.py develop
+```
+
+# Usage
+
+```
+cd vft-data-sanitizer/vds
+python sanitize.py <folder_with_xml_files> <output_folder>
+```
--- vds/fuzzy.py
+++ vds/fuzzy.py
@@ -7,6 +7,7 @@ from collections import defaultdict, Counter
 
 
 def group_by(kv_pairs):
+    """Group key-value pairs by key"""
     groups = defaultdict(list)
     for k, v in kv_pairs:
         groups[k].append(v)
@@ -14,21 +15,25 @@ def group_by(kv_pairs):
 
 
 def ngrams(text, n=3):
+    """Return list of text n-grams of size n"""
     return {text[i:i + n].lower() for i in range(len(text) - n + 1)}
 
 
-def create_index(filename):
-    sid2name = load_sid2name(filename)
-    return group_by((ngram, sid) for sid, name in sid2name for ngram in ngrams(name))
-
-
-def find(search_str, index):
-    sgrams = ngrams(search_str)
+def find(query, index):
+    """Find best match of query string in n-gram index"""
+    sgrams = ngrams(query)
     matches = Counter(sid for ngram in sgrams for sid in index.get(ngram, []))
     return matches.most_common(1)
 
 
+def create_index(filename):
+    """Create n-gram index that maps n-grams to subject ids"""
+    sid2name = load_sid2name(filename)
+    return group_by((ngram, sid) for sid, name in sid2name for ngram in ngrams(name))
+
+
 def load_sid2name(filename):
+    """Load file that maps subject ids to names"""
     with open(filename) as csvfile:
         reader = csv.reader(csvfile)
         next(reader)  # skip header
@@ -36,18 +41,12 @@ def load_sid2name(filename):
             yield sid.rjust(6, '0'), name.replace(',', ' ')
 
 
-def run():
-    for sid, name in load_sid2name('examples/subject_ids.csv'):
-        print(sid, name)
-
-
 if __name__ == '__main__':
     print('running')
-    index = create_index('examples/subject_ids.csv')
+    index = create_index('../data/subject_ids.csv')
     while 1:
-        search_str = input('search: ')
-        if search_str == 'q': break
+        query = input('search: ')
+        if query == 'q': break
         print('-' * 50)
-        match = find(search_str, index)
-        if match:
-            print(match[0])
+        match = find(query, index)
+        print(match[0] if match else 'NO MATCH')
